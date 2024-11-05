282e1145f443c5767549f79730e0a41ae7f69fcc
cjrochest <cjrochester24@gmail.com>
2024-11-04 22:01:45 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_9l8jIRlxl', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '2blj7a4rupphbdsqr20ivslti8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-1
+        region: 'us-east-1' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
